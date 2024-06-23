a8f5e988fb68d9071d01831584c86cd81b6890c6
David E. Bato bato <149306718+DavidBatoDev@users.noreply.github.com>
2024-06-23 15:40:01 +0800

    chore: Add Flask Bcrypt for password hashing, created user model, added database

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -3,25 +3,31 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_cors import CORS
 from config import Config
+from flask_bcrypt import Bcrypt
 
 db = SQLAlchemy()
 migrate = Migrate()
+bcrypt = Bcrypt()
 
 def create_app():
     app = Flask(__name__)
     CORS(app)
     app.config.from_object(Config)
 
+
     db.init_app(app)
     migrate.init_app(app, db)
+    bcrypt.init_app(app)
 
-    from .routes.auth import auth_bp
-    from .routes.notes import notes_bp
-    from .routes.templates import templates_bp
-    from .routes.calendar import calendar_bp
-    from .routes.tasks import tasks_bp
-    from .routes.flashcards import flashcards_bp
-    from .routes.collaboration import collaboration_bp
+    from app import models
+
+    # from .routes.auth import auth_bp
+    # from .routes.notes import notes_bp
+    # from .routes.templates import templates_bp
+    # from .routes.calendar import calendar_bp
+    # from .routes.tasks import tasks_bp
+    # from .routes.flashcards import flashcards_bp
+    # from .routes.collaboration import collaboration_bp
 
     # app.register_blueprint(auth_bp)
     # app.register_blueprint(notes_bp)
