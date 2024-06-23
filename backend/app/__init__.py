61f5c95527c592db3dc03d64a925000e4818bcfb
David E. Bato bato <149306718+DavidBatoDev@users.noreply.github.com>
2024-06-23 16:09:16 +0800

    chore: Add JWTManager for token-based authentication
    added register and login authentication

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -4,24 +4,26 @@ from flask_migrate import Migrate
 from flask_cors import CORS
 from config import Config
 from flask_bcrypt import Bcrypt
+from flask_jwt_extended import JWTManager
 
 db = SQLAlchemy()
 migrate = Migrate()
 bcrypt = Bcrypt()
+jwt = JWTManager()
 
 def create_app():
     app = Flask(__name__)
     CORS(app)
     app.config.from_object(Config)
 
-
     db.init_app(app)
     migrate.init_app(app, db)
     bcrypt.init_app(app)
+    jwt.init_app(app)
 
     from app import models
 
-    # from .routes.auth import auth_bp
+    from .routes.auth import auth_bp
     # from .routes.notes import notes_bp
     # from .routes.templates import templates_bp
     # from .routes.calendar import calendar_bp
@@ -29,7 +31,7 @@ def create_app():
     # from .routes.flashcards import flashcards_bp
     # from .routes.collaboration import collaboration_bp
 
-    # app.register_blueprint(auth_bp)
+    app.register_blueprint(auth_bp)
     # app.register_blueprint(notes_bp)
     # app.register_blueprint(templates_bp)
     # app.register_blueprint(calendar_bp)
